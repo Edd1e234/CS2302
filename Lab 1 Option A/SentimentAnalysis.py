3049bc978cf456b51f2340840ba82e4e58206f51
Eddie Garcia <edd1e234g@gmail.com>
2019-09-09 22:03:50 -0600

    Finished testing

--- Lab 1 Option A/SentimentAnalysis.py
+++ Lab 1 Option A/SentimentAnalysis.py
@@ -3,8 +3,6 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import praw
 import ssl
 
-# Lists
-
 # Setup
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -41,18 +39,32 @@ def get_submission_comments(url):
 
 
 def process_comments(comments, index, lists):
+    # Checks if the appropriate data types are passed.
+    if not isinstance(comments, praw.models.comment_forest.CommentForest) and \
+            isinstance(lists, list) and isinstance(lists[0], list) \
+            and isinstance(lists[1], list) and isinstance(lists[2], list):
+        return
+    if not isinstance(index, int):
+        return
+
     # Base Case.
     if comments is None:
         return
     if index >= len(comments):
         return
 
+    # Adds to the actual list.
     process_string(comments[index].body, lists)
+
     process_comments(comments[index].replies, 0, lists)
     process_comments(comments, index + 1, lists)
 
 
 def process_string(body, lists):
+    """
+    Will add given string 'body' to either negative, positive, or neutral lists based on
+    the highest probability in the 'values' list.
+    """
     # Mapped the same as the lists index's.
     values = [get_text_negative_proba(body),
               get_text_positive_proba(body),
@@ -73,8 +85,8 @@ def process_string(body, lists):
 
 def test_case_1():
     """
-    Tests Case 1 tests that each comment is evaluated.
-    It tests that each lists is being filled accordingly.
+    Tests Case 1 is just a smaller version of the given test case.
+    I'm checking that each list is being filled accordingly.
     """
     comments = get_submission_comments(
         'https://www.reddit.com/user/Edd1e234/comments/czg123/test_case_1/')
@@ -84,23 +96,62 @@ def test_case_1():
     process_comments(comments, 0, lists)
 
     print("Test Case 1")
-    print('Expected: 3, Actual: ', len(negative_list))
-    print('Expected: 2, Actual: ', len(neutral_list))
-    print('Expected: 2, Actual: ', len(positive_list))
+    print('Expected Negative: 3, Actual: ', len(negative_list))
+    print('Expected Positive: 2, Actual: ', len(neutral_list))
+    print('Expected Neutral: 2, Actual: ', len(positive_list))
 
 
 def test_case_2():
     """
-    Tests Case 1 tests that each comment is evaluated.
-    It tests that each lists is being filled accordingly.
+    Tests Case 2 tests that each nested comment is evaluated.
+    It tests that each lists is being filled accordingly. Only neutral comments should
+    have data.
     """
     comments = get_submission_comments(
-        'https://www.reddit.com/user/Edd1e234/comments/czg123/test_case_1/')
+        'https://www.reddit.com/user/Edd1e234/comments/d21mre/neutral_comments/')
     # Output Lists.
     negative_list, positive_list, neutral_list = [], [], []
     lists = [negative_list, positive_list, neutral_list]
     process_comments(comments, 0, lists)
+    print("Test Case 2")
+    print('Expected Negative: 0, Actual: ', len(negative_list))
+    print('Expected Positive: 6, Actual: ', len(neutral_list))
+    print('Expected Neutral: 0, Actual: ', len(positive_list))
 
+
+def test_case_3():
+    """
+    Tests Case 3 tests that the program does not crash if there is no comments.
+    """
+    comments = get_submission_comments(
+        'https://www.reddit.com/user/Edd1e234/comments/d21r2o/empty_link/')
+    # Output Lists.
+    negative_list, positive_list, neutral_list = [], [], []
+    lists = [negative_list, positive_list, neutral_list]
+    process_comments(comments, 0, lists)
+    print("Test Case 3")
+    print('Expected Negative: 0, Actual: ', len(negative_list))
+    print('Expected Positive: 6, Actual: ', len(neutral_list))
+    print('Expected Neutral: 0, Actual: ', len(positive_list))
+
+
+def test_case_4():
+    """
+    Tests that the program will not fail if the user does not pass correct data types,
+    the program should simply not run.
+    """
+    comments = get_submission_comments(
+        'https://www.reddit.com/user/Edd1e234/comments/d21r2o/empty_link/')
+    negative_list, positive_list, neutral_list = [], [], []
+    lists = [negative_list, positive_list, neutral_list]
+    process_comments("not comment", 0, lists)
+    process_comments(comments, "not an index", lists)
+    process_comments(comments, 0, "Not a list")
+
+    lists[0] = "not a list"
+    process_comments(comments, 0, lists)
+
+    print("\nTest case 4 did not fail.")
 
 
 def main():
@@ -108,6 +159,8 @@ def main():
         'https://www.reddit.com/r/learnprogramming/comments/5w50g5/eli5_what_is_recursion/')
     print(comments[0].body)
     print(comments[0].replies[0].body)
+
+    print(type(comments))
 
     # Output Lists.
     negative_list, positive_list, neutral_list = [], [], []
@@ -120,6 +173,9 @@ def main():
     print('Neutral comments amount: ', len(neutral_list), '\n')
 
     test_case_1()
+    test_case_2()
+    test_case_3()
+    test_case_4()
 
 
 main()
