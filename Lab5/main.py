3f6138aaf3c1eedc334bd89262abb49a39985336
Eddie Garcia <edd1e234g@gmail.com>
2019-11-20 22:16:51 -0700

    Added DataWrapperHeap file, made problem 2 faster.

--- Lab5/main.py
+++ Lab5/main.py
@@ -2,160 +2,8 @@ import math
 import time
 
 from data_structures.hash_table.lru_cache import LRUCache
-from data_structures.Heaps.Heaps import MaxHeap
 from data_structures.DataWrapper import DataWrapper
-
-
-class WordHeap(MaxHeap):
-    """
-    This class requires you to use 'DataWrapper' object to insert.
-    """
-
-    def parent(self, i):
-        if i == 0:
-            return -math.inf
-        return i - 1 // 2
-
-    def left_child(self, i):
-        c = 2 * i + 1
-        if c >= len(self.tree):
-            return -math.inf
-        return c
-
-    def right_child(self, i):
-        c = 2 * i + 2
-        if c >= len(self.tree):
-            return -math.inf
-        return c
-
-    def insert(self, item):
-        if not isinstance(item, DataWrapper):
-            print("Needs to be 'DataWrapper' object. ")
-            return
-        for i in range(len(self.tree)):
-            if self.tree[i].object_data == item.object_data:
-                self.tree[i].key += 1
-                self._percolate_up(i)
-                return
-        # We don't need to percolate up anymore.
-        self.tree.append(item)
-
-    def _percolate_up(self, i):
-        if i == 0:
-            return
-
-        parent_index = (i - 1) // 2
-
-        if self.tree[parent_index].key < self.tree[i].key:
-            self.tree[i], self.tree[parent_index] = self.tree[parent_index], self.tree[i]
-            self._percolate_up(parent_index)
-        if self.tree[parent_index].key == self.tree[i].key:
-            if self.tree[parent_index].object_data > self.tree[i]:
-                self.tree[i], self.tree[parent_index] = self.tree[parent_index], self.tree[i]
-                self._percolate_up(parent_index)
-
-    def extract_max(self):
-        if len(self.tree) < 1:
-            return None
-        if len(self.tree) == 1:
-            return self.tree.pop()
-        root = self.tree[0]
-        self.tree[0] = self.tree.pop()
-        self._percolate_down(0)
-        return root
-
-    def _percolate_down(self, i):
-        left_child_index = self.left_child(i)
-        right_child_index = self.right_child(i)
-
-        if left_child_index == -math.inf and right_child_index == -math.inf:
-            return
-
-        # TODO(Edd1e234): Refactor this, needs to be cleaner.
-        if left_child_index == -math.inf and right_child_index != -math.inf:
-            if self.tree[i].key == self.tree[right_child_index].key:
-                if self.tree[i].object_data < self.tree[right_child_index].object_data:
-                    self.tree[i], self.tree[right_child_index] = self.tree[right_child_index], self.tree[i]
-                    self._percolate_down(right_child_index)
-                    return
-            elif self.tree[i].key < self.tree[right_child_index].key:
-                self.tree[i], self.tree[right_child_index] = self.tree[right_child_index], self.tree[i]
-                self._percolate_down(right_child_index)
-                return
-            else:
-                return
-        if right_child_index == -math.inf and left_child_index != -math.inf:
-            if self.tree[i].key == self.tree[left_child_index].key:
-                if self.tree[i].object_data < self.tree[left_child_index].object_data:
-                    self.tree[i], self.tree[left_child_index] = self.tree[left_child_index], self.tree[i]
-                    self._percolate_down(right_child_index)
-                    return
-            elif self.tree[i].key < self.tree[left_child_index].key:
-                self.tree[i], self.tree[left_child_index] = self.tree[left_child_index], self.tree[i]
-                self._percolate_down(left_child_index)
-                return
-            else:
-                return
-
-        # TODO(Edd1e234): Find the bug so that these if blocks are not necessary.
-        # These two if blocks are here for a small bug that has yet to be found.
-        if left_child_index == -math.inf:
-            return
-        if right_child_index == -math.inf:
-            return
-        if self.tree[i].key > max(self.tree[left_child_index].key, self.tree[right_child_index].key):
-            return
-
-        max_child_index = None
-        if self.tree[left_child_index].key == self.tree[right_child_index].key:
-            if self.tree[left_child_index].object_data < self.tree[right_child_index].object_data:
-                max_child_index = 2 * i + 1
-        elif self.tree[left_child_index].key > self.tree[right_child_index].key:
-            max_child_index = 2 * i + 1
-        else:
-            max_child_index = 2 * i + 2
-
-        if max_child_index is None:
-            return
-        if max_child_index >= len(self.tree):
-            return
-
-        self.tree[i], self.tree[max_child_index] = self.tree[max_child_index], self.tree[i]
-        self._percolate_down(max_child_index)
-
-    def is_valid(self):
-        for i in range(1, len(self.tree)):
-            if self.tree[(i - 1) // 2].key < self.tree[i] and \
-                    self.tree[(i - 1) // 2] != self.tree[i]:
-                return False
-        return True
-
-    def print_k(self, k):
-        if not isinstance(k, int):
-            return
-
-        for i in range(k):
-            item = self.extract_max()
-            if item is not None:
-                print(item.object_data, item.key)
-            else:
-                return
-
-
-def problem_2(words, k):
-    word_heap = WordHeap()
-
-    for i in words:
-        item = DataWrapper(1, i)
-        word_heap.insert(item)
-
-    word_heap.print_k(k)
-
-
-def problem_2_print(word_heap):
-    for i in range(len(word_heap.tree)):
-        item = word_heap.extract_max()
-        print(item.object_data, item.key)
+from Lab5.DataWrapperHeap import WordHeap
 
 
 def problem_1():
@@ -170,31 +18,57 @@ def problem_1():
     lru_cache.nodes.print_list()
 
 
+def _get_heap(word_dict):
+    """
+    Receives dict 'word_dict', words as keys and values as int. The values represent the amount
+    time the word appeared in the given list.
+    :param word_dict: (string, int)
+    :return: MaxHeap, with the frequent word being at the top.
+    """
+    word_heap = WordHeap()
+
+    for i in word_dict:
+        item = DataWrapper(word_dict[i], i)
+        word_heap.insert(item)
+    return word_heap
+
+
+def _get_dict(words):
+    hash_table = dict()
+    for word in words:
+        if word in hash_table:
+            hash_table[word] += 1
+        else:
+            hash_table[word] = 1
+    return hash_table
+
+
+def problem_2(words, k):
+    hast_table = _get_dict(words)
+    word_heap = _get_heap(hast_table)
+    word_heap.print_k(k)
+
+
+# Test Case 1.
 def run_problem_2(amount, k):
-    # This list is 8.
     words = ["Eddie", "Billy", "Jimmy", "Eddie", "Eddie", "Billy", "Hey", "Tim"]
+    all_words = []
+    while amount != 0:
+        amount += -1
 
-    full_words = []
-    for i in range(amount):
+        # Adding words.
         for word in words:
-            full_words.append(word)
+            all_words.append(word)
+    start_time = time.time()
+    problem_2(all_words, k)
+    end_time = time.time()
 
-    total_time_start = time.time()
-    problem_2(full_words, k)
-    total_time_end = time.time()
+    # Printing amount of time it took.
+    print("Time:", end_time - start_time)
 
-    print("Time to complete", total_time_end - total_time_start)
 
 def main():
-    print("Tests...")
-
-    run_problem_2(40, 4)
-    run_problem_2(90, 4)
-    run_problem_2(900, 4)
     run_problem_2(9000, 4)
     run_problem_2(90000, 4)
-    run_problem_2(9000000, 4)
-    run_problem_2(90000000, 4)
-
 
 main()
